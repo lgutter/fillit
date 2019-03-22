213ad4f3b6aa7a84d01d8f3b991c3d8e857f7c7f
aholster <aholster@student.codam.nl>
2019-03-22 17:29:18 +0100

    solver update

--- solver.c
+++ solver.c
@@ -6,20 +6,45 @@
 /*   By: aholster <aholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/22 16:40:34 by aholster       #+#    #+#                */
-/*   Updated: 2019/03/22 16:51:04 by aholster      ########   odam.nl         */
+/*   Updated: 2019/03/22 17:10:56 by aholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	map_control(unsigned int *tet, short tetcount)
+void	map_control(unsigned int *tet, short tetcount)
 {
-	short	di;
+	unsigned short	di;
+	unsigned short	*map;
+	unsigned short	index;
 
-	di = smallest_map();
+	index = 0;
+	di = smallest_map(tetcount);
+	map = (unsigned short *)malloc(sizeof(unsigned short *) * 16);
+	if (map == NULL)
+		ft_error();
+	while (index < 16)
+	{
+		map[index] = 0;
+		index++;
+	}
+	while (solve(tet, 0, map, di) == 0)
+	{
+		di++;
+	}
 }
 
-int	solve(unsigned int *tet, short tetcount, unsigned short *map, short di)
+int	solve(unsigned int *tet, short curtet, unsigned short *map, unsigned short di)
 {
+	int	ret;
 
+	if (tet[curtet] == 0)
+		return (42);
+	while (tet_place(tet[curtet], map, di) == 1)
+	{
+		ret = solve(tet, curtet + 1, map, di);
+		if (ret == 42)
+			print_result(tet, map, di);
+	}
+	return (0);
 }
