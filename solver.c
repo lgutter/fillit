7ee0b94dcd02c88d8396ddaff4adce7f5db81aa2
aholster <aholster@student.codam.nl>
2019-03-22 16:55:12 +0100

    testcommit for solver

---
+++ solver.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   solver.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/22 16:40:34 by aholster       #+#    #+#                */
+/*   Updated: 2019/03/22 16:51:04 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int	map_control(unsigned int *tet, short tetcount)
+{
+	short	di;
+
+	di = smallest_map();
+}
+
+int	solve(unsigned int *tet, short tetcount, unsigned short *map, short di)
+{
+
+}
