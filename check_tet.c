07a4530e77f392c23058a903ce9811032b9c8d68
Liewe Gutter <lgutter@student.codam.nl>
2019-03-25 18:04:02 +0100

    Update check_tet.c
    Changed to be able to modify the pointed tet, so check_tet can call shoft_cornet.

--- check_tet.c
+++ check_tet.c
@@ -12,7 +12,7 @@
 
 #include "fillit.h"
 
-int		check_tet(unsigned int tet)
+int		check_tet(unsigned int *tet)
 {
 	unsigned int	temp;
 	short			check;
@@ -20,16 +20,17 @@ int		check_tet(unsigned int tet)
 
 	count = 0;
 	check = 12;
+	shift_corner(tet);
 	while (check >= 0)
 	{
-		temp = (tet >> check) & 15;
+		temp = (*tet >> check) & 15;
 		if ((temp < 6 && temp != 3) || temp == 8)
 		{
-			if ((temp + 1 == (tet & 15)) || (temp + 1 == ((tet >> 4) & 15))
-				|| (temp + 1 == ((tet >> 8) & 15)))
+			if ((temp + 1 == (*tet & 15)) || (temp + 1 == ((*tet >> 4) & 15))
+				|| (temp + 1 == ((*tet >> 8) & 15)))
 				count++;
-			if ((temp + 4 == (tet & 15)) || (temp + 4 == ((tet >> 4) & 15))
-				|| (temp + 4 == ((tet >> 8) & 15)))
+			if ((temp + 4 == (*tet & 15)) || (temp + 4 == ((*tet >> 4) & 15))
+				|| (temp + 4 == ((*tet >> 8) & 15)))
 				count++;
 		}
 		check -= 4;
