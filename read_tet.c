5b1c48fec5b17fd2fa63f3e8888b3c6f236f7cdf
Liewe Gutter <lgutter@student.codam.nl>
2019-03-25 18:02:15 +0100

    Create read_tet.c
    Created final version of read_tet

---
+++ read_tet.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   read_tet.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/25 16:26:49 by lgutter       #+#    #+#                 */
+/*   Updated: 2019/03/25 16:26:50 by lgutter       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int		read_tet(unsigned int *tet, char *buff)
+{
+	short i;
+	short hash;
+	
+	i = 0;
+	hash = 0;
+	if (buff[19] != '\n')
+		return(-1);
+	while (i < 20)
+	{
+		while(buff[i] == '.')
+			i++;
+		if (buff[i] == '#' && hash < 4)
+		{
+			hash++;
+			*tet = (*tet << 4) | (i - ((i + 1) / 5));
+		}
+		else if ((i + 1) % 5 != 0 || ((i + 1) % 5 == 0 && buff[i] != '\n'))
+			return(-1);
+		i++;
+	}
+	if (hash != 4)
+		return (-1);
+	return (1);
+}
