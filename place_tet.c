84e339a243b504e0b20e23eb0d9b17888f19cecc
Liewe Gutter <lgutter@student.codam.nl>
2019-03-26 19:28:18 +0100

    Integrated check_hash
    integrated check_hash into place_tet for optimisation purposes.

--- place_tet.c
+++ place_tet.c
@@ -12,12 +12,27 @@
 
 #include "fillit.h"
 
-int		place_tet(unsigned int *tet, unsigned short *map, unsigned short di)
+static int	check_hash(short hash, unsigned int tet, unsigned short *map,\
+			   unsigned short di)
 {
-	while (check_hash(1, *tet, map, di) != 1 ||
-			check_hash(2, *tet, map, di) != 1 ||
-			check_hash(3, *tet, map, di) != 1 ||
-			check_hash(4, *tet, map, di) != 1)
+	unsigned char	y;
+	unsigned char	x;
+	
+	hash = 0;
+	while (hash <= 12)
+	{
+		y = (((tet >> 16) & 255) + ((tet >> (hash) & 3)));
+		x = ((tet >> 24) + ((tet >> ((hash) + 2)) & 3));
+		if (x >= di || y >= di || ((map[y] >> x) & 1) != 0)
+			return (-1);
+		hash += 4;
+	}
+	return (1);
+}
+
+int			place_tet(unsigned int *tet, unsigned short *map, unsigned short di)
+{
+	while (check_hash(0, *tet, map, di) != 1)
 	{
 		if (increment_offset(tet, di) == -1)
 			return (-1);
