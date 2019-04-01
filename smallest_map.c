b4168a8760a153a00252321860aaab01fa7d0003
aholster <aholster@student.codam.nl>
2019-04-01 15:56:48 +0200

    optimization project concluded
    due to sustained effort by glorious party officials, revolutionary efforts to reduce bureacratic bloat have resulted in superior administrative effiency.
    this long and glorious campaign has recently finalized smallest_map, which has been trimmed down by aforementioned titanic efforts.
    
    however despite these heroic and selfless actions by party officials, effiency continues to suffer under legacy implentations and mismanaged, far fetched "optimization" projects wich saw several fixes haveing to implemented several times as rollbacks erased past progress.
    
    Fillit Shall Prevail!

--- smallest_map.c
+++ smallest_map.c
@@ -6,32 +6,22 @@
 /*   By: aholster <aholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/15 16:26:43 by lgutter        #+#    #+#                */
-/*   Updated: 2019/03/24 18:42:22 by aholster      ########   odam.nl         */
+/*   Updated: 2019/04/01 15:49:33 by aholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned short	smallest_map(short tetcount)
 {
-	if (tetcount < 5)
-	{
-		if (tetcount < 4)
-			return (tetcount + 1);
-		return (4);
-	}
+
 	if (tetcount < 13)
 	{
-		if (tetcount > 6 && tetcount < 10)
-			return (6);
+		if (tetcount < 5)
+			return ((tetcount < 4) ? tetcount + 1 : 4);
 		if (tetcount < 7)
 			return (5);
-		if (tetcount > 9)
-			return (7);
+		return ((tetcount < 10) ? 6 : 7);
 	}
-	if (tetcount < 17)
-		return (8);
 	if (tetcount < 21)
-		return (9);
-	if (tetcount < 26)
-		return (10);
-	return (11);
+		return ((tetcount < 17) ? 8 : 9);
+	return ((tetcount < 26) ? 10 : 11);
 }
